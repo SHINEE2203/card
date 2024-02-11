bc0d15722dbe0a1c7363a2c7076884dba514f7ba
shinee2203 <shineevasuden@gmail.com>
2024-02-11 11:54:33 +0530

    weather

---
+++ script.js
@@ -0,0 +1,30 @@
+const apiKey = 'fcf0927543f998315eb22b7193b9c8e2';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationTnput = document.getElementById('locationTnput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+    const location = locationTnput.value;
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => { console.log(data)
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
